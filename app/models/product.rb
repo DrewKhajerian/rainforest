cedd0144045811edd6dacc45cad9222f6c3a1ed0
DrewKhajerian <arkhajer@gmail.com>
2013-07-08 14:16:29 -0400

    product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price_in_cents
+
+  validates :description, :name, :precence => true
+  validtaes :price_in_cents, :numericality => {only_integer => true}
 end
