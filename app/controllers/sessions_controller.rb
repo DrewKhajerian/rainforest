ffac1ae4ac0e53acc84389572d4ee0e52474ab9b
DrewKhajerian <arkhajer@gmail.com>
2013-07-08 17:35:04 -0400

    added flash alerts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to products_url, :notice => "Logged in!"
     else
+    	flash.now[:alert] = "Invalid email or password"
       render "new"
     end
   end
