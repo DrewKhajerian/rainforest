a68cfeac7bdabc85b92bfd0a88e610a311ea400e
DrewKhajerian <arkhajer@gmail.com>
2013-07-09 13:37:32 -0400

    added asspciations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
   validates_presence_of :password, :on => :create
+
+  has_many :reviews
+  has_many :products, :through => :reviews
 end
